d5e31db5f76b3edcfc2d7d5bf123eeb2bf48653c
Casey Celestin <caseycelestin@Caseys-MacBook-Pro-2.local>
2019-01-31 14:21:44 -0500

    Began to implement SpinOut class, untested

--- spinout.cpp
+++ spinout.cpp
@@ -1,2 +1,46 @@
 // Implementation file of Spinout Class
-//
+
+#include "spinout.hpp"
+
+#include <string>
+
+namespace cs427_527
+{
+	SpinOut::SpinOut(std::string& s)
+	{
+		puzzle = s;
+	}
+	
+	bool isLegalMove(int i)
+	{
+		return true;
+	}
+	
+	void makeMove(int i)
+	{
+		
+	}
+	
+	int totalMoves() const
+	{
+		return total_moves;	
+	}
+	
+	bool isSolved() const
+	{
+		if (puzzle.compare("-------") == 0)
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+	
+	std::string toString()
+	{
+		return puzzle;
+	}
+}
+
--- spinout.hpp
+++ spinout.hpp
@@ -1,7 +1,46 @@
-// Header file for Spinout Class
 
 #ifndef __SPINOUT_HPP__
 #define __SPINOUT_HPP__
 
+#include <string>
+
+namespace cs427_527
+{
+	class SpinOut
+	{
+	public:
+		// SpinOut constructor
+		// Takes a string pointer from user input, initializes "///////" if none
+		SpinOut(std::string& s = "///////");
+		
+		// Checks if move is legal
+		// Takes int 0 to 6 inclusive, numbered left to right 0123456
+		bool isLegalMove(int i);
+		
+		// Executes move
+		// Checks to see if move is legal, then rotates to other orientation
+		void makeMove(int i);
+		
+		// Returns number of legal moves completed
+		int totalMoves() const;
+		
+		// Check to see if puzzle solved
+		// Looking for "-------"
+		bool isSolved() const;
+		
+		// Returns string representation of puzzle
+		std::string toString(); 
+		
+	private:
+		// Size of puzzle
+		static const int SIZE = 7;
+		
+		// String representation of puzzle
+		std::string puzzle;
+		
+		// Number of moves completed
+		int total_moves = 0;
+	}
+}
 
 #endif
