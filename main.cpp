67ac00fd24bf432095d70b19f9b81f23cf71665e
Casey Celestin <caseycelestin@Caseys-MBP-2.wireless.yale.internal>
2019-02-04 12:37:33 -0500

    Added non interactive mode, no known bugs

--- main.cpp
+++ main.cpp
@@ -38,6 +38,44 @@ void interactive(string* init, int size)
 
 }
 
+void non_interactive(string* init, int size)
+{
+	SpinOut game;
+	int index;
+	int m = 1;
+	if(init[1].find('/') != string::npos || init[1].find('-') != string::npos)
+	{
+		game = SpinOut{init[1]};
+		index = 1;
+	} else 
+	{
+		game = SpinOut{};	
+		index = 0;
+	}
+
+	while (++index < size) 
+	{
+		if(!game.isLegalMove(std::stoi(init[index])))
+		{
+			cout << "Spinout: illegal move " << init[index] << " in position " << m << " for " << game.toString() << endl;
+			return;
+		} else
+		{
+			game.makeMove(std::stoi(init[index]));
+			m++;
+		}
+	}
+
+	if (game.isSolved())
+	{
+		cout << "SOLVED" << endl;
+	} else
+	{
+		cout << game.toString() << endl;
+	}
+}
+
+
 int main(int argc, char **argv)
 {
 	string* input = new string[argc];
@@ -51,6 +89,9 @@ int main(int argc, char **argv)
 	if (input[1].compare("-i") == 0)
 	{
 		interactive(input, argc);
+	} else
+	{
+		non_interactive(input, argc);
 	}
 
 	delete[] input;	
